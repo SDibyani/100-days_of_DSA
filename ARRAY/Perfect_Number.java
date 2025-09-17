68b4316935c021ddf562a80df2536c927aea80b0
Dibyani <sdibyani123@gmail.com>
2025-09-17 21:25:33 +0530

    Added Perfect_Number.java into ARRAY folder

---
+++ ARRAY/Perfect_Number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Perfect_Number {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int sum=0;
+        for(int i=1; i < number; i++){
+            if( number % i ==0){
+                sum = sum + i;
+            }
+        }
+        if( sum == number){
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not an perfect Number");
+
+        }
+        sc.close();
+    }
+    
+}
